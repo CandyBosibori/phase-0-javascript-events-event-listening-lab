e5d052a9b2a61819a321ead641b2af3066e0871d
Candy <candy.ongaro@student.moringaschool.com>
2024-01-02 10:16:34 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
+// const button = document.getElementById("button")
+
+// function deleteHandler(e){
+//     e.target.remove();
+// }
+
+// function addingEventListener() {
+//      button.addEventListener("clicked", deleteHandler)
+
+// }
+const button = document.getElementById("button");
+
+function deleteHandler(e) {
+    e.target.remove();
+}
+
 function addingEventListener() {
+    button.addEventListener("click", deleteHandler);
 }
